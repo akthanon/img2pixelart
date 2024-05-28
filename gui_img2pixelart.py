b1b4944c7c790343dbe49c8cf7de54af4473468a
Calyseym <94359345+calyseym@users.noreply.github.com>
2024-05-28 07:46:31 -0700

    Add files via upload

--- gui_img2pixelart.py
+++ gui_img2pixelart.py
@@ -1,8 +1,24 @@
 import tkinter as tk
 from tkinter import filedialog, messagebox
-from tkinter import ttk
 import subprocess
 import os
+
+index_slider = None  # Inicializamos index_slider como una variable global
+
+def update_index_slider_color():
+    global index_slider  # Indicamos que queremos utilizar la variable global index_slider
+    if selected_palette.get() == "*clusterizar*":
+        if index_slider is not None:  # Verificamos si index_slider ha sido creado
+            index_slider.config(troughcolor="green")
+    else:
+        if index_slider is not None:  # Verificamos si index_slider ha sido creado
+            index_slider.config(troughcolor="gray")
+
+
+def select_palette(palette_name):
+    selected_palette.set(palette_name)
+    toggle_index_option()
+    update_index_slider_color()  # Llama a la función para actualizar el color del slider
 
 def select_input_folder():
     folder_path = filedialog.askdirectory()
@@ -27,6 +43,20 @@ def update_image_size(img_size):
     entry_image_size.delete(0, tk.END)
     entry_image_size.insert(tk.END, img_size)
 
+def toggle_index_option():
+    global index_slider
+    if selected_palette.get() == "*clusterizar*":
+        entry_index.config(state="normal")
+        if index_slider is not None:  # Verificamos si index_slider ha sido creado
+            index_slider.config(troughcolor="green")
+            index_slider.config(state="normal")
+    else:
+        entry_index.config(state="disabled")
+        if index_slider is not None:  # Verificamos si index_slider ha sido creado
+            index_slider.config(troughcolor="gray")
+            index_slider.config(state="disabled")
+    update_index_slider_color()  # Actualiza el color del slider
+
 def process_images():
     # Change button text to "Processing..."
     button_process.config(text="Processing...")
@@ -39,13 +69,18 @@ def process_images():
     scale_factor = entry_scale_factor.get()
     image_size = entry_image_size.get()
     input_image = entry_input_image.get()
+    index = str(index_slider.get())  # Cambia aquí para obtener el valor del slider
 
     if not input_folder and not input_image:
+        button_process.config(text="ERROR")
         messagebox.showerror("Error", "Debe seleccionar una carpeta de entrada o una imagen de entrada.")
+        button_process.config(text="Process Images")
         return
 
     if input_folder and input_image:
+        button_process.config(text="ERROR")
         messagebox.showerror("Error", "Solo se puede seleccionar una carpeta de entrada o una imagen de entrada, no ambas.")
+        button_process.config(text="Process Images")
         return
 
     command = ["python", "img2pixelart.py"]
@@ -53,7 +88,7 @@ def process_images():
     if output_folder:
         command.extend(["-o", output_folder])
 
-    if palette_name:
+    if palette_name and palette_name!="*clusterizar*":
         command.extend(["-p", palette_name])
 
     if scale_factor_option.get() == 1:
@@ -68,6 +103,10 @@ def process_images():
     else:
         command.extend(["-i", input_image])
 
+    # Verifica si el index_slider está habilitado y agrega el argumento "-n" seguido del valor del slider al comando
+    if index_slider is not None and index_slider.cget('state') != 'disabled':
+        command.extend(["-n", index])
+
     try:
         subprocess.run(command, check=True)
         messagebox.showinfo("Proceso terminado", "El procesamiento de imágenes ha finalizado correctamente.")
@@ -75,8 +114,16 @@ def process_images():
         messagebox.showerror("Error", f"Error al procesar las imágenes: {e}")
 
     # Change button text back to "Process Images" after processing is finished
+
     button_process.config(text="Process Images")
     root.update()
+    print("Codigo Ejecutado:")
+    for i in command:
+        print(i, end=" ")
+    print("")
+    print("")
+    print("En espera de la siguiente instrución...")
+
 
 
 def clear_input_text():
@@ -112,41 +159,53 @@ def toggle_scale_factor_option():
         entry_image_size.config(state="normal")
         image_size_slider.config(state="normal")
 
+def toggle_index_option():
+    if selected_palette.get() == "*clusterizar*":
+        entry_index.config(state="normal")
+        if index_slider is not None:  # Verificamos si index_slider ha sido creado
+            index_slider.config(troughcolor="green")
+            index_slider.config(state="normal")
+    else:
+        entry_index.config(state="disabled")
+        if index_slider is not None:  # Verificamos si index_slider ha sido creado
+            index_slider.config(troughcolor="gray")
+            index_slider.config(state="disabled")
+
 root = tk.Tk()
 root.title("Pixel Art Converter")
 
 # Input choice check buttons
 input_choice = tk.IntVar()
-input_choice.set(1)  # Default to Input Folder
+input_choice.set(0)  # Default to Input Folder
 
-input_folder_checkbutton = tk.Checkbutton(root, text="Input Folder", variable=input_choice, onvalue=1, offvalue=2, command=toggle_input_option)
-input_folder_checkbutton.grid(row=0, column=0, sticky="w")
+input_image_checkbutton = tk.Checkbutton(root, text="Input Image", variable=input_choice, onvalue=0, offvalue=1, command=toggle_input_option)
+input_image_checkbutton.grid(row=0, column=0, sticky="w")
 
-input_image_checkbutton = tk.Checkbutton(root, text="Input Image", variable=input_choice, onvalue=2, offvalue=1, command=toggle_input_option)
-input_image_checkbutton.grid(row=1, column=0, sticky="w")
-
-# Input folder
-label_input_folder = tk.Label(root, text="Input Folder:")
-label_input_folder.grid(row=0, column=1)
-entry_input_folder = tk.Entry(root, width=50)
-entry_input_folder.grid(row=0, column=2)
-button_input_folder = tk.Button(root, text="Select", command=select_input_folder)
-button_input_folder.grid(row=0, column=3)
+input_folder_checkbutton = tk.Checkbutton(root, text="Input Folder", variable=input_choice, onvalue=1, offvalue=0, command=toggle_input_option)
+input_folder_checkbutton.grid(row=1, column=0, sticky="w")
 
 # Input image
-label_input_image = tk.Label(root, text="Input Image:")
-label_input_image.grid(row=1, column=1)
+label_input_image = tk.Label(root, text=":")
+label_input_image.grid(row=0, column=1)
 entry_input_image = tk.Entry(root, width=50)
-entry_input_image.grid(row=1, column=2)
-button_input_image = tk.Button(root, text="Select", command=select_input_image)
-button_input_image.grid(row=1, column=3)
+entry_input_image.grid(row=0, column=2)
+button_input_image = tk.Button(root, text="*   Select   *", command=select_input_image)
+button_input_image.grid(row=0, column=3)
+
+# Input folder
+label_input_folder = tk.Label(root, text=":")
+label_input_folder.grid(row=1, column=1)
+entry_input_folder = tk.Entry(root, width=50)
+entry_input_folder.grid(row=1, column=2)
+button_input_folder = tk.Button(root, text="*   Select   *", command=select_input_folder)
+button_input_folder.grid(row=1, column=3)
 
 # Output folder
 label_output_folder = tk.Label(root, text="Output Folder:")
 label_output_folder.grid(row=2, column=0, columnspan=2)
 entry_output_folder = tk.Entry(root, width=50)
 entry_output_folder.grid(row=2, column=2)
-button_output_folder = tk.Button(root, text="Select", command=select_output_folder)
+button_output_folder = tk.Button(root, text="*   Select   *", command=select_output_folder)
 button_output_folder.grid(row=2, column=3)
 
 # Palette name
@@ -154,12 +213,23 @@ label_palette_name = tk.Label(root, text="Palette Name:")
 label_palette_name.grid(row=3, column=0, columnspan=2)
 
 palette_folder = "paletas"
-palettes = ["original"] + [p[:-4] for p in os.listdir(palette_folder) if p.endswith(".png")]
+palettes = ["*clusterizar*", "original"] + [p[:-4] for p in os.listdir(palette_folder) if p.endswith(".png")]
 selected_palette = tk.StringVar(root)
 selected_palette.set(palettes[0])  # Default value
 
-palette_menu = tk.OptionMenu(root, selected_palette, *palettes)
+palette_menu = tk.OptionMenu(root, selected_palette, *palettes, command=select_palette)
 palette_menu.grid(row=3, column=2)
+
+# Add the following line to set the initial color of index_slider
+update_index_slider_color() 
+
+# Indexar Menu
+entry_index = tk.Entry(root, width=5)
+entry_index.insert(tk.END, "32")
+
+index_slider = tk.Scale(root, from_=0, to=128, orient="horizontal", resolution=4, length=200)
+index_slider.grid(row=3, column=3)
+index_slider.set(32)
 
 # Scale factor choice check buttons
 scale_factor_option = tk.IntVar()
@@ -172,7 +242,7 @@ image_size_checkbutton = tk.Checkbutton(root, text="Image Size", variable=scale_
 image_size_checkbutton.grid(row=5, column=0, sticky="w")
 
 # Scale factor
-label_scale_factor = tk.Label(root, text="Scale Factor:")
+label_scale_factor = tk.Label(root, text="")
 label_scale_factor.grid(row=4, column=1)
 
 entry_scale_factor = tk.Entry(root, width=10)
@@ -184,16 +254,16 @@ scale_factor_slider.grid(row=4, column=3)
 scale_factor_slider.set(0.2)
 
 # Image size
-label_image_size = tk.Label(root, text="Image Size:")
+label_image_size = tk.Label(root, text="")
 label_image_size.grid(row=5, column=1)
 
 entry_image_size = tk.Entry(root, width=10)
 entry_image_size.grid(row=5, column=2)
-entry_image_size.insert(tk.END, "128")
+entry_image_size.insert(tk.END, "256")
 
 image_size_slider = tk.Scale(root, from_=16, to=800, orient="horizontal", resolution=16, length=200, command=update_image_size)
 image_size_slider.grid(row=5, column=3)
-image_size_slider.set(128)
+image_size_slider.set(256)
 
 # Process button
 button_process = tk.Button(root, text="Process Images", command=process_images)
@@ -201,5 +271,6 @@ button_process.grid(row=6, column=2)
 
 toggle_input_option()  # Ensure initial state is correct
 toggle_scale_factor_option()  # Ensure initial state is correct
+toggle_index_option()  # Ensure initial state is correct
 
 root.mainloop()
--- img2pixelart.py
+++ img2pixelart.py
@@ -10,15 +10,49 @@ def denoise_image(img):
     img_np = np.array(img.convert('RGBA'))
     rgb_np = img_np[..., :3]
     alpha_np = img_np[..., 3]
-    denoised_rgb_np = cv2.bilateralFilter(rgb_np, d=20, sigmaColor=75, sigmaSpace=75)
+    denoised_rgb_np = cv2.bilateralFilter(rgb_np, d=2, sigmaColor=75, sigmaSpace=75)
     denoised_img_np = np.dstack((denoised_rgb_np, alpha_np))
     denoised_img = Image.fromarray(denoised_img_np)
     return denoised_img
 
+def indexar_colores(image, n_clusters):
+    # Convertir la imagen a modo RGB si no lo está
+    if image.mode != 'RGB':
+        image = image.convert('RGB')
+
+    # Convertir la imagen a una matriz numpy
+    image_array = np.array(image)
+
+    # Obtener la forma de la matriz de la imagen
+    height, width, channels = image_array.shape
+
+    # Redimensionar la matriz a una forma plana para que cada píxel se represente como un vector de características
+    flattened_image_array = image_array.reshape((height * width, channels))
+
+    # Inicializar el modelo KMeans con el número de clusters especificado
+    kmeans = KMeans(n_clusters=n_clusters, random_state=0)
+
+    # Ajustar el modelo a los datos de la imagen
+    kmeans.fit(flattened_image_array)
+
+    # Obtener las etiquetas de los clústeres para cada píxel
+    labels = kmeans.predict(flattened_image_array)
+
+    # Asignar los colores de los clústeres a cada píxel en la imagen
+    clustered_image = kmeans.cluster_centers_[labels]
+
+    # Reformar la imagen para que tenga la misma forma que la imagen original
+    clustered_image = clustered_image.reshape((height, width, channels))
+
+    # Convertir la matriz numpy de vuelta a una imagen de PIL
+    clustered_image = Image.fromarray(clustered_image.astype('uint8'), 'RGB')
+
+    return clustered_image
+
 def reduce_image(image, scale_factor=None, image_size=None):
     img = np.array(image)
     original_height, original_width = img.shape[:2]
-    
+
     if image_size is not None:
         max_dimension = max(original_width, original_height)
         if original_width > original_height:
@@ -32,8 +66,9 @@ def reduce_image(image, scale_factor=None, image_size=None):
         new_height = int(original_height * scale_factor)
     else:
         new_width, new_height = original_width, original_height
-    
-    small_img = cv2.resize(img, (new_width, new_height), interpolation=cv2.INTER_NEAREST)
+
+    # Usar cv2.INTER_AREA para reducir la imagen
+    small_img = cv2.resize(img, (new_width, new_height), interpolation=cv2.INTER_AREA)
     small_image = Image.fromarray(small_img)
     return small_image
 
@@ -155,6 +190,73 @@ def process_one_image(filename, output_folder, palette_image_path, palette_name,
             
         print(f'Processed {input_path} and saved to {final_output_path}')
 
+def process_images_cluster(input_folder, output_folder, palette_image_path, palette_name, scale_factor, image_size, n_clusters):
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+    
+    if palette_image_path:
+        palette_image = Image.open(palette_image_path)
+        palette = extract_palette(palette_image)
+    else:
+        palette = None
+    
+    for filename in os.listdir(input_folder):
+        if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+            input_path = os.path.join(input_folder, filename)
+            name, ext = os.path.splitext(filename)
+            output_filename = f"{palette_name}_{name}{ext}"
+            output_path = os.path.join(output_folder, output_filename)
+            original_image = Image.open(input_path)
+            denoised_image = denoise_image(original_image)
+            small_image = reduce_image(denoised_image, scale_factor=scale_factor, image_size=image_size)
+            
+            imagen_indexada = indexar_colores(small_image, n_clusters)
+            
+            original_size = (original_image.width, original_image.height)
+
+            pixel_art_image = enlarge_image(imagen_indexada, original_size)
+
+            final_output_path = os.path.splitext(output_path)[0] + '.png'
+
+            if pixel_art_image.mode == 'RGBA':
+                pixel_art_image.save(final_output_path, 'PNG')
+            else:
+                pixel_art_image.save(output_path, 'JPEG')
+            
+            print(f'Processed {input_path} and saved to {final_output_path}')
+
+def process_one_image_cluster(filename, output_folder, palette_image_path, palette_name, scale_factor, image_size, n_clusters):
+    input_folder = os.getcwd()
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+    
+    if palette_image_path:
+        palette_image = Image.open(palette_image_path)
+        palette = extract_palette(palette_image)
+    else:
+        palette = None
+    
+    if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+        input_path = os.path.join(input_folder, filename)
+        name, ext = os.path.splitext(os.path.basename(filename))
+        output_filename = f"{palette_name}_{name}.png"
+        output_path = os.path.join(output_folder, output_filename)
+        original_image = Image.open(input_path)
+
+        # Aplicar filtro de suavizado para reducir el ruido
+        denoised_image = denoise_image(original_image)
+
+        small_image = reduce_image(denoised_image, scale_factor=scale_factor, image_size=image_size)
+        imagen_indexada = indexar_colores(small_image, n_clusters)
+
+        original_size = (original_image.width, original_image.height)
+        pixel_art_image = enlarge_image(imagen_indexada, original_size)
+
+        # Guardar como PNG para evitar artefactos de compresión JPEG
+        final_output_path = output_path
+        pixel_art_image.save(final_output_path, 'PNG')
+        print(f'Processed {input_path} and saved to {final_output_path}')
+
 def create_output_directory(output_folder):
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
@@ -168,6 +270,7 @@ if __name__ == "__main__":
     parser.add_argument("-d", "--input_folder", help="Nombre de directorio de entrada.")
     parser.add_argument("-o", "--output_folder", help="Nombre de directorio de salida.")
     parser.add_argument("-i", "--input_image", help="Nombre del archivo de imagen.")
+    parser.add_argument("-n", "--clusters_number", help="Numero de colores a indexar con clusters.")
 
     args = parser.parse_args()
 
@@ -179,8 +282,12 @@ if __name__ == "__main__":
             print(palette)
         exit()
 
+
     if args.scale_factor is not None and args.image_size is not None:
-        parser.error("No se pueden usar ambos argumentos -f (factor de escala) y -i (tamaño de la imagen) al mismo tiempo.")
+        parser.error("No se pueden usar ambos argumentos -f (factor de escala) y -s (tamaño de la imagen) al mismo tiempo.")
+
+    if args.palette_name is not None and args.clusters_number is not None:
+        parser.error("No se pueden usar ambos argumentos -p (palette_name) y -n (numero de clusters) al mismo tiempo.")
 
     if args.input_folder is not None and args.input_image is not None:
         parser.error("No se pueden usar ambos argumentos -d (directorio de imagenes) y -i (nombre de la imagen) al mismo tiempo.")
@@ -188,8 +295,8 @@ if __name__ == "__main__":
     if args.input_folder is None and args.input_image is None:
         parser.error("Se tiene que utilizar al menos uno de los argumentos -d (directorio de imagenes) o -i (nombre de la imagen).")
 
-    if args.scale_factor is None and args.image_size is None and args.palette_name is None:
-        parser.error("Debe proporcionar al menos un argumento entre -f (factor de escala), -i (tamaño de la imagen) o -p (nombre de la paleta).")
+    if args.scale_factor is None and args.image_size is None and args.palette_name is None and args.clusters_number is None:
+        parser.error("Debe proporcionar al menos un argumento entre -f (factor de escala), -i (tamaño de la imagen) o -p (nombre de la paleta) o -n (numero de clusters).")
 
     output_folder = 'output_pixelart'
     palette_folder = 'paletas'
@@ -207,7 +314,8 @@ if __name__ == "__main__":
     if args.output_folder:
         create_output_directory(args.output_folder)
         output_folder = args.output_folder
-    if args.palette_name is not None and args.palette_name!="original":
+
+    if args.palette_name is not None and args.palette_name!="original" and args.clusters_number is None:
         palette_image_path = os.path.join(palette_folder, args.palette_name + '.png')
         if not os.path.exists(palette_image_path):
             print(f"Paleta {args.palette_name} no encontrada. Usando la paleta por defecto 'nes'.")
@@ -217,14 +325,27 @@ if __name__ == "__main__":
             palette_name = args.palette_name
     else:
         palette_image_path = None
-        palette_name = 'original'
+        if args.clusters_number is None:
+            palette_name = 'original'
+        else:
+            palette_name = 'clusters'
+            clusters_number=int(args.clusters_number)
 
-    if args.input_folder is not None:
+    if args.input_folder is not None and args.clusters_number is None:
          process_images(input_folder, output_folder, palette_image_path, palette_name, args.scale_factor, args.image_size)
          print("COMPLETADO")
 
-    if args.input_image is not None :
+    if args.input_image is not None and args.clusters_number is None:
         process_one_image(input_image, output_folder, palette_image_path, palette_name, args.scale_factor, args.image_size)
         print("COMPLETADO")
+
+    if args.input_folder is not None and args.clusters_number is not None:
+         process_images_cluster(input_folder, output_folder, palette_image_path, palette_name, args.scale_factor, args.image_size, clusters_number)
+         print("COMPLETADO")
+
+    if args.input_image is not None and args.clusters_number is not None:
+        process_one_image_cluster(input_image, output_folder, palette_image_path, palette_name, args.scale_factor, args.image_size, clusters_number)
+        print("COMPLETADO")
+
 
     
