874e91597a8051bffef4f3a64e71e51d6b619d75
Calyseym <94359345+calyseym@users.noreply.github.com>
2024-05-29 03:43:21 -0700

    add update code

---
+++ update.bat
@@ -0,0 +1,45 @@
+@echo off
+setlocal
+
+REM Ruta del directorio donde se descargará y descomprimirá el repositorio
+set REPO_DIR=%cd%
+
+REM URL del repositorio de GitHub
+set GITHUB_REPO=https://github.com/calyseym/img2pixelart
+
+REM URL del archivo zip de la última versión del repositorio
+set ZIP_URL=%GITHUB_REPO%/archive/refs/heads/main.zip
+
+REM Nombre del archivo zip descargado
+set ZIP_FILE=img2pixelart.zip
+
+REM Descarga el archivo zip usando curl
+echo Descargando la última versión del repositorio...
+curl -L -o %ZIP_FILE% %ZIP_URL%
+
+REM Verifica si la descarga fue exitosa
+if %errorlevel% neq 0 (
+    echo Error al descargar el archivo.
+    exit /b 1
+)
+
+REM Elimina el directorio anterior si existe
+if exist %REPO_DIR% (
+    echo Eliminando la versión anterior...
+    rmdir /S /Q %REPO_DIR%
+)
+
+REM Descomprime el archivo zip descargado
+echo Descomprimiendo el archivo...
+tar -xf %ZIP_FILE% > nul
+
+REM Mueve los archivos a la carpeta deseada
+move img2pixelart-main %REPO_DIR%
+
+REM Elimina el archivo zip descargado
+del %ZIP_FILE%
+
+echo Actualización completada.
+pause
+
+endlocal
