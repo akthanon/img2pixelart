34062b5fc048abc054b33fbc090b3cfc09c90a70
Calyseym <94359345+calyseym@users.noreply.github.com>
2024-05-31 01:07:48 -0700

    update update file

--- update.bat
+++ update.bat
@@ -1,6 +1,21 @@
 @echo off
 setlocal
 
+REM Verifica si curl está instalado
+curl --version >nul 2>&1
+set CURL_INSTALLED=%errorlevel%
+
+REM Verifica si wget está instalado
+wget --version >nul 2>&1
+set WGET_INSTALLED=%errorlevel%
+
+REM Verifica si se tiene curl o wget
+if %CURL_INSTALLED% neq 0 if %WGET_INSTALLED% neq 0 (
+    echo Error: No se encontró curl ni wget.
+    echo Por favor, instale curl o wget y vuelva a intentarlo.
+    exit /b 1
+)
+
 REM Ruta del directorio donde se descargará y descomprimirá el repositorio
 set REPO_DIR=%cd%\img2pixelart
 echo %REPO_DIR%
@@ -14,9 +29,14 @@ set ZIP_URL=%GITHUB_REPO%/archive/refs/heads/main.zip
 REM Nombre del archivo zip descargado
 set ZIP_FILE=img2pixelart.zip
 
-REM Descarga el archivo zip usando curl
-echo Descargando la última versión del repositorio...
-curl -L -o %ZIP_FILE% %ZIP_URL%
+REM Descarga el archivo zip usando curl si está disponible, de lo contrario usa wget
+if %CURL_INSTALLED% equ 0 (
+    echo Descargando la última versión del repositorio con curl...
+    curl -L -o %ZIP_FILE% %ZIP_URL%
+) else (
+    echo Descargando la última versión del repositorio con wget...
+    wget -O %ZIP_FILE% %ZIP_URL%
+)
 
 REM Verifica si la descarga fue exitosa
 if %errorlevel% neq 0 (
@@ -34,8 +54,12 @@ REM Descomprime el archivo zip descargado
 echo Descomprimiendo el archivo...
 tar -xf %ZIP_FILE% > nul
 
+REM Elimina el archivo de actualizaicón para evitar errores, este es el único archivo que no se sobreescribe
+del img2pixelart-main\update.bat
+
 REM Mueve los archivos a la carpeta deseada
-move img2pixelart-main main_code
+xcopy img2pixelart-main\* . /E /H /Y 
+rd /s /q img2pixelart-main
 
 REM Elimina el archivo zip descargado
 del %ZIP_FILE%
